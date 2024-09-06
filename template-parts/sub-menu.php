fd22a30d0f0b43e81cfd281a01777049415539d5
ayumi <dottoz2531@gmail.com>
2024-09-06 23:48:21 +0900

    no message

---
+++ template-parts/sub-menu.php
@@ -0,0 +1,28 @@
+<nav class="gallery__sub-menu">
+        <div class="sub-menu__inner">
+          <ul class="sub-items">
+            <div class="sub-items1">
+              <li class="sub-item">
+                <a href="" class="item_link">CAKE</a>
+              </li>
+              <li class="sub-item sub-item--type2">
+                <a href="" class="item-link">COOKIE</a>
+              </li>
+              <li class="sub-item sub-item--type3">
+                <a href="" class="item_link">CUPCAKE</a>
+              </li>
+            </div>
+            <div class="sub-items2">
+              <li class="sub-item sub-item--type4">
+                <a href="" class="item_link">EVENT</a>
+              </li>
+              <li class="sub-item sub-item--type5">
+                <a href="" class="item_link">OSHIKATU</a>
+              </li>
+              <li class="sub-item sub-item--type6">
+                <a href="" class="item_link">OTHER</a>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </nav>
