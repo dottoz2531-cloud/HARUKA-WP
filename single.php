dd44b0e3f77c6d9b75ff22031bd19ad4b2757048
ayumi <dottoz2531@gmail.com>
2024-09-13 23:10:42 +0900

    no message

--- single.php
+++ single.php
@@ -6,21 +6,21 @@
           <ul class="sub-items">
             <div class="sub-items1">
               <li class="sub-item">
-                <a href="" class="item_link">CAKE</a>
+                <a href="<?php the_permalink(); ?>" class="item_link">CAKE</a>
               </li>
               <li class="sub-item sub-item--type2">
-                <a href="" class="item-link">COOKIE</a>
+                <a href="<?php the_permalink(); ?>" class="item-link">COOKIE</a>
               </li>
               <li class="sub-item sub-item--type3">
-                <a href="" class="item_link">CUPCAKE</a>
+                <a href="<?php the_permalink(); ?>" class="item_link">CUPCAKE</a>
               </li>
             </div>
             <div class="sub-items2">
               <li class="sub-item sub-item--type4">
-                <a href="" class="item_link">EVENT</a>
+                <a href="<?php the_permalink(); ?>" class="item_link">EVENT</a>
               </li>
               <li class="sub-item sub-item--type5">
-                <a href="" class="item_link">OSHIKATU</a>
+                <a href="<?php the_permalink(); ?>" class="item_link">OSHIKATU</a>
               </li>
               <li class="sub-item sub-item--type6">
                 <a href="" class="item_link">OTHER</a>
