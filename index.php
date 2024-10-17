f5a0470e2eb3640608bff072f077f19ca2423289
ayumi <dottoz2531@gmail.com>
2024-10-18 00:51:02 +0900

    index.php genre 追加

--- index.php
+++ index.php
@@ -11,7 +11,10 @@
 
       <section id="concept" class="concept">
         <div class="concept__inner inner">
-          <h2 class="heading concept__heading js-in-view fade-in-up">CONCEPT</h2>
+        <div class="heading js-in-view fade-in-up">
+              <h2 class="heading-main">CONCEPT</h2>
+              <p class="heading-sub">コンセプト</p>
+            </div>
           <div class="concept__slider">
             <!-- Slider main container -->
             <div id="js-concept-swiper" class="swiper concept__swiper">
@@ -79,98 +82,140 @@
 
       <section id="gallery" class="gallery">
         <div class="gallery__inner inner">
-          <h2 class="heading heading--type2 js-in-view fade-in-up">GALLERY</h2>
-          <div class="gallery__cards">
-          <?php if(have_posts()): ?>
-                      <?php while(have_posts()) :?>
-                        <?php the_post(); ?>
-            <a href="<?php the_permalink(); ?>" class="gallery__card gallery-card">
-              <div class="gallery-card__img">
-              <?php the_post_thumbnail(); ?>
+          <div class="gallery__box">
+            <div class="heading js-in-view fade-in-up">
+              <h2 class="heading-main">GALLERY</h2>
+              <p class="heading-sub">ギャラリー</p>
+            </div>
+            <div class="item">
+              <div class="item__title js-in-view fade-in-up">
+                  <h3 class="title-main">ITEM</h3>
+                  <p class="title-sub">アイテム</p>
               </div>
-              <?php 
-               $category = get_the_category();?>
-              <div class="gallery-card__text <?php echo $category[0]->slug; ?>">
-                <p class="card-text">
-               <?php echo $category[0]->cat_name; ?></p>
-              </div>
-            </a>
-            <?php endwhile;?>
-           <?php endif;?>
+               <div class="item__cards">
+                 <?php if(have_posts()): ?>
+                   <?php while(have_posts()) :?>
+                      <?php the_post(); ?>
+                        <a href="<?php the_permalink(); ?>" class="item__card item-card">
+                           <div class="item-card__img">
+                             <?php the_post_thumbnail(); ?>
+                           </div>
+                           <?php
+                            $category = get_the_category();?>
+                           <div class="item-card__text <?php echo $category[0]->slug; ?>">
+                             <p class="card-text">
+                             <?php echo $category[0]->cat_name; ?></p>
+                           </div>
+                        </a>
+                    <?php endwhile;?>
+                 <?php endif;?>
+                </div>
+            </div>
           </div>
-        </div>
-      </section>
-
-      <section class="genre">
-        <div class="genre__inner">
-          <h2 class="heading heading--type2 js-in-view fade-in-up">GENRES</h2>
-          <div class="genre__cards">
-            <a href="" class="genre__card genre-card">
-              <div class="genre-card__img">
-                <img
-                  src="<?php echo get_template_directory_uri()?>/img/img-gallery/cake.png"
-                  alt=""
-                />
+          <div class="genre">
+              <div class="item__title js-in-view fade-in-up">
+                <h3 class="title-main">GENRE</h3>
+                <p class="title-sub">ジャンル</p>
               </div>
-              <div class="genre-card__text">
-                <p class="genre-text">WEDDING</p>
+              <div class="genre__cards">
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/cake.png" alt="" />
+                  </div>
+                  <div class="genre-card__text event">
+                    <p class="genre-text">EVENT</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text wedding">
+                    <p class="genre-text">WEDDING</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text baby">
+                    <p class="genre-text">BABY</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text ribon">
+                    <p class="genre-text">RIBON</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text flower">
+                    <p class="genre-text">FLOWER</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text threed">
+                    <p class="genre-text">3D</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text oshikatu">
+                    <p class="genre-text">OSHIKATU</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text vehicle">
+                    <p class="genre-text">VEHICLE</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img vehicle">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text animal">
+                    <p class="genre-text">ANIMAL</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text fruits">
+                    <p class="genre-text">MANY FRUITS</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text saengle">
+                    <p class="genre-text">SAENGLE CAKE</p>
+                  </div>
+                </a>
+                <a href="" class="genre__card genre-card">
+                  <div class="genre-card__img">
+                    <img src="<?php echo get_template_directory_uri()?>/img/img-gallery/others.png" alt="" />
+                  </div>
+                  <div class="genre-card__text kawaii">
+                    <p class="genre-text">KAWAII</p>
+                  </div>
+                </a>
               </div>
-            </a>
-            <a href="" class="genre__card genre-card">
-              <div class="genre-card__img">
-                <img
-                  src="<?php echo get_template_directory_uri()?>/img/img-gallery/cake.png"
-                  alt=""
-                />
-              </div>
-              <div class="genre-card__text">
-                <p class="genre-text">WEDDING</p>
-              </div>
-            </a>
-            <a href="" class="genre__card genre-card">
-              <div class="genre-card__img">
-                <img
-                  src="<?php echo get_template_directory_uri()?>/img/img-gallery/cake.png"
-                  alt=""
-                />
-              </div>
-              <div class="genre-card__text">
-                <p class="genre-text">WEDDING</p>
-              </div>
-            </a>
-            <a href="" class="genre__card genre-card">
-              <div class="genre-card__img">
-                <img
-                  src="<?php echo get_template_directory_uri()?>/img/img-gallery/cake.png"
-                  alt=""
-                />
-              </div>
-              <div class="genre-card__text">
-                <p class="genre-text">WEDDING</p>
-              </div>
-            </a>
-            <a href="" class="genre__card genre-card">
-              <div class="genre-card__img">
-                <img
-                  src="<?php echo get_template_directory_uri()?>/img/img-gallery/cake.png"
-                  alt=""
-                />
-              </div>
-              <div class="genre-card__text">
-                <p class="genre-text">WEDDING</p>
-              </div>
-            </a>
-            <a href="" class="genre__card genre-card">
-              <div class="genre-card__img">
-                <img
-                  src="<?php echo get_template_directory_uri()?>/img/img-gallery/cake.png"
-                  alt=""
-                />
-              </div>
-              <div class="genre-card__text">
-                <p class="genre-text">WEDDING</p>
-              </div>
-            </a>
+            </div>
           </div>
         </div>
       </section>
